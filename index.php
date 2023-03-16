93abbe6c52c0ce3ba13bbaf618d1b5749c7cd950
Annabell Noora <128036101+TechAnnabell@users.noreply.github.com>
2023-03-16 11:26:44 +0100

    Update index.php - SQL Cut up

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
-<?php
-$servername = "localhost";
-$username = "";
-$password = "";
-$dbname = "";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-?>
 <html>
     <head>
         <title>Home | TechAnnabell</title>
